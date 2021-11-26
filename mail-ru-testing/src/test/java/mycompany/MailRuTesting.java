fc00b773b3192b21a918334a28b1dbad7fb5633f
Kamotsky Yuri <kamockii1977@gmail.com>
2021-11-26 22:02:04 +0300

    new

--- mail-ru-testing/src/test/java/mycompany/MailRuTesting.java
+++ mail-ru-testing/src/test/java/mycompany/MailRuTesting.java
@@ -28,21 +28,22 @@ public class MailRuTesting {
         open("https://mail.ru/");
 
         //Переход на страницу почты
-        $x("//*[@id='ph-whiteline']/div/div[1]/a[2]").click();
+        $x("//a[@class='ph-project svelte-1dxh3mc' and text()='Почта']").click();
 
         //Авторизация в почте
         $("input[name='username']").setValue("testing-mailbox");
-        $("div[class='box-0-2-119 activeBox-0-2-124']").click();
-        $("span[class='inner-0-2-89 innerTextWrapper-0-2-90']").click();
+        $("div.box-0-2-119.activeBox-0-2-124").click();
+        $("button.base-0-2-87.primary-0-2-101.auto-0-2-113").click(); //span.inner-0-2-89.innerTextWrapper-0-2-90
         $("input[name='password']").setValue("testing12345");
         $("path[fill-rule='evenodd']").click();
-        $("span[class='inner-0-2-89 innerTextWrapper-0-2-90']").click();
+        $("button.base-0-2-87.primary-0-2-101.auto-0-2-113").click(); //span.inner-0-2-89.innerTextWrapper-0-2-90
 
         //Написать письмо себе
-        $("div[class='compose-dropdown']").shouldBe(Condition.visible, Duration.ofSeconds(15)).click();
-        $(byText("Написать себе")).click();
+        $("div.compose-dropdown").shouldBe(Condition.visible, Duration.ofSeconds(15)).click();
+        $("div.list-item.list-item_hover-support.list-item_pure").click();
         $("input[class='container--H9L5q size_s--3_M-_'][type='text'][name='Subject'][tabindex='400'][value]").setValue("Letter for me");
-        $x("/html/body/div[15]/div[2]/div/div[1]/div[2]/div[3]/div[5]/div/div/div[2]/div[1]/div[1]").setValue("Hello");
-        $x("/html/body/div[15]/div[2]/div/div[2]/div[1]/span[1]/span/span").click();
+        //$x("//div[@class='editable-gfix cke_editable cke_editable_inline cke_contents_true cke_show_borders' and @tabindex='505']").setValue("Hello");
+        $("div.editable-9o3a.cke_editable.cke_editable_inline.cke_contents_true.cke_show_borders>div").setValue("Hello");
+        $x("//span[@class='button2__txt' and text()='Отправить']").click();
     }
 }
